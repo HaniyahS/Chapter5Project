54f03341932594e9979cd048f2bec3e5e19ee788
HaniyahS <lionroar87453@gmail.com>
2025-07-12 03:28:35 -0400

    Iineup*unfinished*

--- Chapter5Project/Chapter5Project.cpp
+++ Chapter5Project/Chapter5Project.cpp
@@ -2,41 +2,36 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
 using namespace std;
 int main()
 {
-	double start, days, increase, population = 0, result, num, pop;
-	cout << "Enter the starting number of organisms: ";
-	cin >> start;
+	string student, first, last, word;
+	int i = 0;
 
-	while (start < 2) {
-		cout << "The starting number cannot be less than 2.\nTry again: ";
-		cin >> start;
-	}
+    vector<std::string> words;
+    ifstream file("LineUp.txt");
+    if (!file) {
+        cout << "Error: File could not be opened!" << std::endl;
+        return 1;
+    }
 
-	cout << "\nEnter the average daily increase as a percentage%: ";
-	cin >> increase;
+    
 
-	while (increase < 0) {
-		cout << "\nThe average cannot be below 0.\nTry again: ";
-		cin >> increase;
-	}
+    string line;
+    cout << "Names from file:\n";
+    while (getline(file,line)) { // Read character by line
+        cout  << line << "\n"; // Print each line
+        line = student;
+        i++;
+    }
+    cout << "\nNumber of students: " << i;
 
-	cout << "\nEnter the number of days: ";
-	cin >> days;
-
-	while (days < 1) {
-		cout << "\nThe number of days cannot be less than 1.\nTry again: ";
-		cin >> days;
-	}
-
-	cout << "\nSize of population for each day: ";
-	cout << "\nDay\t\tPopulation\n";
-
-	for (int i = 0; i <= days; i++) {
-		result = increase / 100;
-		num = start + (result * i);
-		pop = population + num;
-		cout << i << "\t\t" << pop << "\n";
-	}
+  
+    cout << "\nfirst student: " << first << "\nlast student: " << last;
+    file.close();
+    return 0;
 }
